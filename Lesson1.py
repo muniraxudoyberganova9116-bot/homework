66d58c3db8e083a416a8b24fa42826a406da69df
zhav3n <zhav3n@users.noreply.github.com>
2026-05-22 01:39:40 +0500

    Update Lesson1.py: added answers and fixes

--- Lesson1.py
+++ Lesson1.py
@@ -1,1 +1,22 @@
+# 1. Why do you think companies analyze large volumes of data?
+print(" Usually analyzing large volumes of data helps companies to understand customer behavior, identify trends and predict future ones. This will help them improve customer satisfaction and profit.")
 
+
+# 2. If analyzing and sorting large data manually in Excel is difficult, how do you think Python can help solve this problem?
+
+print("In excel, it takes longer time to input all data manually and high probably, it will have erros. But in Python, we can create a program to automate the data analysis process and sorting them.")
+print("So, python is better than excel to shorten the process time and reduce the probability of errors.")
+
+
+# 3. Imagine you work at a sales company that receives data about 10,000 customer transactions daily. How would you analyze this data?
+print("I don't exactly know how to analyze this date with the help of python, but i am sure i will be able to build the program to analyze it so that as a user, i will only input the data and get the results without worrying about the process of analyzing it.")
+
+#4. In your opinion, what tasks can Python be useful for in BI processes? (e.g., data cleaning, visualization, etc.)
+print("Python can be useful for data cleaning, data analysis, data visualization, and machine learning. It has many libraries such as pandas for data manipulation, matplotlib and seaborn for data visualization, and scikit-learn for machine learning. These libraries make it easier to handle large datasets and extract insights from them.")
+print("The sentence above was written by AI to be honest. I need some time to explore the role of coding in business a little bit more to be able to answer this question by myself.")
+
+# 5. If you wanted to compare a company's profit year by year, how could this be done using Python?
+print("i should do some research on these questions as i am very new for business intelligence even though i have some experience in coding. ")
+
+# 6. If you don't know Python, what difficulties might you face when working with large datasets?
+print("I have some basic knowledge of python, but my expected probrlem would be how to find the most logical and easiest way to analyze the date without having errors. ")
