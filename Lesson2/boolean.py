c07038ab94008f9f1430239ff513afc763d40dfd
zhav3n <zhav3n@users.noreply.github.com>
2026-05-24 10:21:44 +0500

    Update boolean.py and remove Lesson2.zip

--- Lesson2/boolean.py
+++ Lesson2/boolean.py
@@ -3,7 +3,7 @@
 #1. Write a program that accepts a username and password and checks if both are not empty.
 username = input("Enter your username: ")
 password = input("Enter your password: ")   
-if username!=" " and password!=" ":
+if username!=" " and password!="":
     print("Username and password are valid.")
 else:
     print("Username and password cannot be empty.")
